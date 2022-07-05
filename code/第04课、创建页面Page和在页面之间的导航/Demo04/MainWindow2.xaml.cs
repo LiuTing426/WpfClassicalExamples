fa29f50d115c32de56a0be4fe4e0d489548479f0
liuting <liuting@lektec.com>
2022-07-05 15:58:54 +0800

    04、创建页面Page和在页面之间的导航

---
+++ code/第04课、创建页面Page和在页面之间的导航/Demo04/MainWindow2.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Demo04
+{
+    /// <summary>
+    /// Main_Window2.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow2 : Window
+    {
+        public MainWindow2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPage1_click(object sender, RoutedEventArgs e)
+        {
+            this.FrameA.Source = new Uri("Page1.xaml", UriKind.RelativeOrAbsolute);
+        }
+
+        private void btnPage2_click(object sender, RoutedEventArgs e)
+        {
+            this.FrameA.Source = new Uri("Page2.xaml", UriKind.RelativeOrAbsolute);
+        }
+
+        private void btnPage3_click(object sender, RoutedEventArgs e)
+        {
+            this.FrameA.Source = new Uri("Page3.xaml", UriKind.RelativeOrAbsolute);
+        }
+    }
+}
---
+++ code/第04课、创建页面Page和在页面之间的导航/Demo04/Page1.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo04
+{
+    /// <summary>
+    /// Page1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 后退
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService.CanGoBack == true)
+            {
+                NavigationService.GoBack();
+            }
+        }
+        /// <summary>
+        /// 前进
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonMove_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService.CanGoForward == true)
+            {
+                NavigationService.GoForward();
+            }
+        }
+        /// <summary>
+        /// 打开Page2
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonPage2_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("Page2.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
