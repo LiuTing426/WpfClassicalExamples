f1e6a415a2302e4a48f78164e3cb148db095a8ae
liuting <liuting@lektec.com>
2022-07-06 15:56:53 +0800

    08、向WPF应用程序传递启动参数，修改App文件

---
+++ code/第08课、向WPF应用程序传递启动参数/Demo08/Demo08/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Demo08
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            //StartupEventArgs ->启动事件参数
+            var arg = e.Args;
+            if (arg.Contains("/Window1"))
+            {
+                //右键 Demo08 --> 属性 -->调试 -->命令行参数 -->写入 /Window1 就会启动Window1
+                new Window1().Show();
+            }
+            else
+            {
+                new MainWindow().Show();
+            }
+        }
+    }
+}
