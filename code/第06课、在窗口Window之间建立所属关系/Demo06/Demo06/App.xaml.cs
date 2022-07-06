cc6ca2821925d3a208ceca1eca4494f1eb37252e
liuting <liuting@lektec.com>
2022-07-06 11:18:14 +0800

    06、在窗口Window之间建立所属关系 --修改App.xaml和代码-- （工具箱在窗体之前）

---
+++ code/第06课、在窗口Window之间建立所属关系/Demo06/Demo06/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Demo06
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        //重写
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //实例化
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            ToolBox toolBox = new ToolBox();
+            //设置所属关系
+            toolBox.Owner = mainWindow;
+            toolBox.Show();
+        }
+    }
+}
