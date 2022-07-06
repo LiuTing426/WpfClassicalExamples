3b8166de7506fb4103580846c5cf5083511ff9d4
liuting <liuting@lektec.com>
2022-07-06 09:47:50 +0800

    73、使用MVVM模式

---
+++ code/第73课、使用MVVM模式/Demo73/Demo73/ViewModels/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Demo73.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo73.ViewModels
+{
+    //① 继承INotifyPropertyChanged
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        //④ 右键 MainViewModel->快速操作和重构 -> 添加构造函数
+        public MainViewModel()
+        {
+            //⑨ 进行初始化
+            UserCollection = new ObservableCollection<MainModel>
+            {
+                new MainModel{FirstName="User",LastName="1"},
+                new MainModel{FirstName="User",LastName="2"},
+                new MainModel{FirstName="User",LastName="3"},
+                new MainModel{FirstName="User",LastName="4"}
+            };
+        }
+
+        //② 实现接口
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // ③ 自己添加
+        public void OnPropertyChanged(string propertyName)
+        {
+            //判断是否为空，不为空就调用invoke
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //⑤ 定义属性
+
+        //⑥ 右键selectedUser ->快速操作和重构 -> 封装字段（但仍使用字段）
+        private MainModel m_selectedUser;
+        //⑦ 右键selectedUser ->快速操作和重构 -> 使用访问器的程序块主体 （两个SelectedUser都点）
+        public MainModel SelectedUser
+        {
+            get { return m_selectedUser; }
+            set
+            {
+                m_selectedUser = value;
+
+                //⑧ 添加自己写的
+                OnPropertyChanged("SelectedUser");
+            }
+        }
+
+
+
+        private ObservableCollection<MainModel> m_userCollection;
+        public ObservableCollection<MainModel> UserCollection
+        {
+            get { return m_userCollection; }
+            set
+            {
+                m_userCollection = value;
+
+                OnPropertyChanged("UserCollection");
+            }
+        }
+
+    }
+}
