b7e2dcd4a917e675b98f353b13ac6279193608d6
liuting <liuting@lektec.com>
2022-07-06 16:32:27 +0800

    09、捕捉WPF应用程序中未处理的异常 App.xaml.cs主要捕捉！！！

---
+++ code/第09课、捕捉WPF应用程序中未处理的异常/Demo09/Demo09/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Demo09
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            //添加 DispatcherUnhandledException +=后Tab键，点应用
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+        // 处理程序中所有的异常
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            //***************
+            //不知道发生什么异常的时候，如果不解决异常可能会导致程序崩溃，
+            //可以在App.xaml.cs中解决
+            //记录日志，记录发生什么异常
+            //如果程序关键或者是内部逻辑错误可以在此处重启程序
+
+            //throw new NotImplementedException();
+            MessageBox.Show("DispatcherUnhandledException, 异常已处理！");
+
+            //在e.Handled之前记录日志等 
+            e.Handled = true;
+        }
+
+
+
+    }
+}
---
+++ code/第09课、捕捉WPF应用程序中未处理的异常/Demo09/Demo09/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo09
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //添加事件 可忽略 此处处理异常记录日志不是最优选择！！！还是要在App.xaml.cs中处理！！！
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        //可忽略
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            //throw new NotImplementedException();
+            MessageBox.Show("CurrentDomain_UnhandledException, 异常已处理！");
+            //e.Handled = true;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (rd_0.IsChecked == true)
+            {
+                try
+                {
+                    throw new Exception("引发异常");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("捕捉异常");
+                }
+            }
+            if (rd_1.IsChecked == true)
+            {
+                throw new Exception("引发异常");
+            }
+
+        }
+    }
+}
