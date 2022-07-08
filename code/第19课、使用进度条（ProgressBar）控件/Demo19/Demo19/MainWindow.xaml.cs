1000638367dc7a8b2da5b1a4d6912b016595a3ca
liuting <liuting@lektec.com>
2022-07-08 13:26:57 +0800

    19、使用进度条（ProgressBar）控件。增减，减少，关闭，开始进度条。

---
+++ code/第19课、使用进度条（ProgressBar）控件/Demo19/Demo19/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo19
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            pb1.Value += 10;
+            pb2.Value += 10;
+        }
+
+        private void Reduce_click(object sender, RoutedEventArgs e)
+        {
+            pb1.Value -= 10;
+            pb2.Value -= 10;
+        }
+
+        private void Start_click(object sender, RoutedEventArgs e)
+        {
+            pb3.IsIndeterminate = true;
+        }
+
+        private void Stop_click(object sender, RoutedEventArgs e)
+        {
+            pb3.IsIndeterminate = false;
+            pb3.Value = 100;
+        }
+    }
+}
