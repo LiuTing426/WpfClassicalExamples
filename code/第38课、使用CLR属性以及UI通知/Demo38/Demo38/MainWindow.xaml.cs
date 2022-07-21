6c97c49e57518bf5de8f0e59c56eededca682a77
liuting <liuting@lektec.com>
2022-07-21 13:52:52 +0800

    38、使用CLR属性以及UI通知

---
+++ code/第38课、使用CLR属性以及UI通知/Demo38/Demo38/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo38
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window,INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string Department
+        {
+            get
+            {
+                return "软件工程部";
+            }
+        }
+
+        public string PersonName
+        {
+            get { return personName; }
+            set
+            {
+                personName = value;
+                //调用
+                OnPropertyChanged("PersonName");
+            }
+        }
+
+        private string personName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            //判断是否为空，不是则调用函数
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// 提交
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("hi" + PersonName);
+        }
+
+        /// <summary>
+        /// 重置
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            PersonName = "";
+        }
+    }
+}
