549be3535a6eccc1be9278098f5e3d5b789431a6
liuting <liuting@lektec.com>
2022-07-21 15:46:42 +0800

    39、使用WPF中的依赖项属性

---
+++ code/第39课、使用WPF中的依赖项属性/Demo39/Demo39/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo39
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //propdp
+        public string PersonName
+        {
+            get { return (string)GetValue(PersonNameProperty); }
+            set { SetValue(PersonNameProperty, value); }
+        }
+
+
+        //typeof(MainWindow) 指当前文件这个类中
+        // Using a DependencyProperty as the backing store for PersonName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PersonNameProperty =
+            DependencyProperty.Register("PersonName", typeof(string), typeof(MainWindow), new PropertyMetadata(""));
+
+        public string Depatrment
+        {
+            get
+            {
+                return "软件部门";
+            }
+        }
+
+        /// <summary>
+        /// 提交
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("hi" + PersonName);
+        }
+
+        /// <summary>
+        /// 重置
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            PersonName = "";
+        }
+    }
+}
