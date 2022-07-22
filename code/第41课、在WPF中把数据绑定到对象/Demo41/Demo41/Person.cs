f6bb59a4f872fe39ef36ea9333e60cdda5f99c8e
liuting <liuting@lektec.com>
2022-07-22 10:00:55 +0800

    41、在WPF中把数据绑定到对象

---
+++ code/第41课、在WPF中把数据绑定到对象/Demo41/Demo41/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo41
+{
+    public class Person
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 博客
+        /// </summary>
+        public string Blog { get; set; }
+
+        /// <summary>
+        /// 经验
+        /// </summary>
+        public int Experience { get; set; }
+    }
+}
