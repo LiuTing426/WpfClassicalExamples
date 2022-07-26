e6cc9b0a0600841b66211e55af8af7795e2da352
liuting <liuting@lektec.com>
2022-07-26 16:50:45 +0800

    55、在WPF中创建和使用用户控件 -- RGBA颜色显示

---
+++ code/第55课、在WPF中创建和使用用户控件/Demo55/Demo55/ColorMixer.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo55
+{
+    /// <summary>
+    /// ColorMixer.xaml 的交互逻辑
+    /// </summary>
+    public partial class ColorMixer : UserControl
+    {
+        public ColorMixer()
+        {
+            InitializeComponent();
+        }
+
+        //propdp
+        public Color SelectedColor
+        {
+            get { return (Color)GetValue(SelectedColorProperty); }
+            set { SetValue(SelectedColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedColor.  This enables animation, styling, binding, etc...
+
+        //SelectedColor 的初始颜色为DarkBlue
+        public static readonly DependencyProperty SelectedColorProperty =
+            DependencyProperty.Register("SelectedColor", typeof(Color), typeof(ColorMixer), new PropertyMetadata(Colors.DarkBlue));
+    }
+}
