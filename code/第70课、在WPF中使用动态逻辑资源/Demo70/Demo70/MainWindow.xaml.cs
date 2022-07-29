4c316998698d5b4c23ed74fc2d287f95afc6c1dd
liuting <liuting@lektec.com>
2022-07-29 10:17:00 +0800

    70、在WPF中使用动态逻辑资源

---
+++ code/第70课、在WPF中使用动态逻辑资源/Demo70/Demo70/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo70
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked_Red(object sender, RoutedEventArgs e)
+        {
+            //资源字典
+            var brush = Resources["myLinearGradientBrush"];
+            if (brush is LinearGradientBrush lBrush)
+            {
+                lBrush = new LinearGradientBrush
+                {
+                    GradientStops = new GradientStopCollection
+                    {
+                        new GradientStop(Colors.LightGoldenrodYellow,0),
+                        new GradientStop(Colors.Red,1)
+                    }
+                };
+                Resources["myLinearGradientBrush"] = lBrush;
+            }
+
+        }
+
+        private void RadioButton_Checked_Green(object sender, RoutedEventArgs e)
+        {
+            var brush = Resources["myLinearGradientBrush"];
+            if (brush is LinearGradientBrush lBrush)
+            {
+                lBrush = new LinearGradientBrush
+                {
+                    GradientStops = new GradientStopCollection
+                    {
+                        new GradientStop(Colors.LightGreen,0),
+                        new GradientStop(Colors.Green,1)
+                    }
+                };
+                Resources["myLinearGradientBrush"] = lBrush;
+            }
+        }
+
+        private void RadioButton_Checked_Blue(object sender, RoutedEventArgs e)
+        {
+            var brush = Resources["myLinearGradientBrush"];
+            if (brush is LinearGradientBrush lBrush)
+            {
+                lBrush = new LinearGradientBrush
+                {
+                    GradientStops = new GradientStopCollection
+                    {
+                        new GradientStop(Colors.LightBlue,0),
+                        new GradientStop(Colors.Blue,1)
+                    }
+                };
+                Resources["myLinearGradientBrush"] = lBrush;
+            }
+        }
+    }
+}
